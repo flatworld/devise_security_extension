ac9003e9b965f272b20f8fce5444de4ab1839302
Po Ki Chui <pchui@flatworldknowledge.com>
2014-11-22 03:21:28 +0800

    added debugging statements

--- lib/devise_security_extension/hooks/session_limitable.rb
+++ lib/devise_security_extension/hooks/session_limitable.rb
@@ -3,6 +3,7 @@
 # and on authentication. Retrieving the user from session (:fetch) does
 # not trigger it.
 Warden::Manager.after_set_user :except => :fetch do |record, warden, options|
+  puts options.inspect
   if options[:mode] != :masquerade &&
      record.respond_to?(:update_unique_session_id!) && warden.authenticated?(options[:scope])
     unique_session_id = Devise.friendly_token
@@ -18,6 +19,7 @@ Warden::Manager.after_set_user :only => :fetch do |record, warden, options|
   scope = options[:scope]
   env   = warden.request.env
 
+  puts options.inspect
   if options[:mode] != :masquerade &&
      record.respond_to?(:unique_session_id) && warden.authenticated?(scope) && options[:store] != false
     if record.unique_session_id != warden.session(scope)['unique_session_id'] && !env['devise.skip_session_limitable']
